e2333f6c3ce55a91cf0ea90d684fb5d74f8ee396
nahuel <nahuel.dimuro@hotmail.com>
2023-10-20 20:07:49 -0300

    fix

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,42 +1,15 @@
 const socketClient = io();
-
 const form = document.getElementById("chatForm");
 const inputMessage = document.getElementById("chatMessage");
-const h3Name = document.getElementById("name");
+const h3Name = document.getElementById("email");
 const divChat = document.getElementById("chat");
 
-let user;
 
-Swal.fire({
-  title: "Welcome",
-  text: "What is your name",
-  input: "text",
-  inputValidator: (value) => {
-    if (!value) {
-      return "Name is required";
-    }
-  },
-  confirmButtonText: "Enter",
-}).then((input) => {
-  user = input.value;
-  h3Name.innerText = `Chat user: ${user}`;
-  socketClient.emit("newUser", user);
-});
-
-socketClient.on("newUserBroadcast", (user) => {
-  Toastify({
-    text: `${user} connected`,
-    duration: 5000,
-    style: {
-      background: "linear-gradient(to right, #00b09b, #96c93d)",
-    },
-  }).showToast();
-});
 
 form.onsubmit = (e) => {
   e.preventDefault();
   const infoMessage = {
-    name: user,
+    email: h3Name.value,
     message: inputMessage.value,
   };
   socketClient.emit("message", infoMessage);
@@ -44,7 +17,7 @@ form.onsubmit = (e) => {
 
 socketClient.on("chat", (messages) => {
   const chat = messages
-    .map((objMessage) => `<p>${objMessage.name}: ${objMessage.message}</p>`)
+    .map((objMessage) => `<p>${objMessage.email}: ${objMessage.message}</p>`)
     .join(" ");
   divChat.innerHTML = chat;
 });
